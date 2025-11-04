debce2ac6acdaa289b8e13573e0cbb13d7916a78
Rajendra-Katuwal <raj.katuwal2061@gmail.com>
2025-11-04 16:32:37 +0545

    feat: completed Task 5

---
+++ Task5/Program.cs
@@ -0,0 +1,15 @@
+﻿DateTime DoB = new DateTime (2004, 12, 02);
+
+DateTime currDateTime = DateTime.Now;
+
+TimeSpan ageSpan = currDateTime - DoB;
+
+
+Console.WriteLine ($"My date of birth is: {DoB.ToShortDateString()}");
+Console.WriteLine ($"Current date is: {currDateTime.ToShortDateString()}");
+Console.WriteLine($"My age in years is: {ageSpan.Days/365} years");
+
+DateTime tenDayAdditionDOB = DoB.AddDays(10);
+Console.WriteLine ($"Date after adding ten days to my DOB is: {tenDayAdditionDOB.ToShortDateString()}");
+
+
