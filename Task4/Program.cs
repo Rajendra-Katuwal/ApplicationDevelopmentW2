428bb226e9e652c502d5c3f774093a2f229e9758
Rajendra-Katuwal <raj.katuwal2061@gmail.com>
2025-11-04 16:21:45 +0545

    feat: completed Task 4

---
+++ Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] favNumbers = { 7, 14, 21, 28, 35 };
+
+// sorting in acending order
+Array.Sort(favNumbers);
+
+// Reversing the array to get descending order
+Array.Reverse(favNumbers);
+
+
+// Printing each number in the array
+Console.WriteLine("Favorite Numbers in Descending Order:");
+int length = favNumbers.Length;
+for (int i = 0; i < length; i++)
+{
+    Console.WriteLine(favNumbers[i]);
+}
+
+
+// Finding the index of a specific number
+int index = Array.IndexOf(favNumbers, 21);
+Console.WriteLine($"\nIndex of 21 in the array: {index}");
