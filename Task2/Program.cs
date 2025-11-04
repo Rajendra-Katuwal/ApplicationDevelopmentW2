3749b10a75d422ffe03689828b4a2f7e18421814
Rajendra-Katuwal <raj.katuwal2061@gmail.com>
2025-11-04 16:06:32 +0545

    feat: completed Task 2

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿class Circle
+{
+    public const double PI = 3.14;
+
+    public double calculateArea(double radius)
+    {
+        double area = PI * radius * radius;
+        return area;
+    }
+
+    public double calculatePerimeter(double radius)
+    {
+        double circumference = 2 * PI * radius;
+        return circumference;
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        // Error: Cannot assign to 'PI' because it is a 'const'
+        Circle.PI = 3.888;
+    }
+}
