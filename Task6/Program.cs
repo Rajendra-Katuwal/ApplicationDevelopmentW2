409a6bdbb23bda6c69d8ee45a0c029f8f9fcd385
Rajendra-Katuwal <raj.katuwal2061@gmail.com>
2025-11-04 16:39:51 +0545

    feat: completed Task 6

---
+++ Task6/Program.cs
@@ -0,0 +1,22 @@
+﻿// List Example
+List <string> favFruits = new List <string> { "Apple", "Banana", "Cherry" };
+favFruits.Add("Tomato");
+favFruits.Remove("Banana");
+
+foreach (var fruit in favFruits)
+{
+    Console.WriteLine(fruit);
+}
+
+
+
+// Dictionary Example
+Dictionary <int, string> fruits = new();
+fruits.Add(1, "Apple");
+fruits.Add(2, "Banana");
+fruits.Add(3, "Cherry");
+
+foreach (var kvp in fruits)
+{
+    Console.WriteLine($"Key: {kvp.Key}, Value: {kvp.Value}");
+}
